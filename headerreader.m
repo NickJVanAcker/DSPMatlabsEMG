7fe78fcd1092305117e26c8fb2453dd762e44070
Seppe Van de Poel <seppevandepoel@hotmail.be>
2022-04-12 11:50:45 +0200

    header read + function to mV implemented in script

--- headerreader.m
+++ headerreader.m
@@ -1,6 +1,8 @@
-%% read in file
+%% read in file + sorting header
 clear;
 filename = 'file1.txt';
+matrix = readmatrix(filename);
+matrix(:,1:2) = [];
 linesOfFile = readlines(filename);
 headerline = linesOfFile(2); % header is on the second line
 headerline = convertStringsToChars(headerline); % needed for further use
@@ -11,3 +13,12 @@ date = headerobject.date;
 time = headerobject.time;
 sampfreq = headerobject.samplingRate;
 resolution = headerobject.resolution;
+
+%% Function 
+n = resolution(1); % not always 16 for each channel. improved version that checks channel per column necessairy?
+VCC = 3;
+gemg = sampfreq;
+emg = (((matrix/2^n) - (1/2)) * VCC) / gemg; %in V
+emg= emg*1000; %to mV
+
+
