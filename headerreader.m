7144797cec0dfa32c54522b2ae3a115b7491f7ba
SeppeVdP <70763433+SeppeVdP@users.noreply.github.com>
2022-05-05 08:43:43 +0200

    fft

--- headerreader.m
+++ headerreader.m
@@ -1,5 +1,7 @@
 %% read in file + sorting header
 clear;
+lowerbandpass = 30;
+upperbandpass = 50;
 filename = 'file1.txt';
 matrix = readmatrix(filename);
 matrix(:,1:2) = [];
@@ -11,14 +13,35 @@ headerline = headerline(1:end-1); %remove chars to be able to read in as jsonobj
 headerobject = jsondecode(headerline);
 date = headerobject.date;
 time = headerobject.time;
-sampfreq = headerobject.samplingRate;
+Fs = headerobject.samplingRate;   %sampfreq
 resolution = headerobject.resolution;
+
+%% Removing outline
+matrix = rmoutliers(matrix,'mean','ThresholdFactor',2); %Change 2 to x for desired outliner removal
 
 %% Function 
 n = resolution(1); % not always 16 for each channel. improved version that checks channel per column necessairy?
 VCC = 3;
-gemg = sampfreq;
+gemg = Fs;
 emg = (((matrix/2^n) - (1/2)) * VCC) / gemg; %in V
 emg= emg*1000; %to mV
 
+
+%%fast fourier transform first column 
+%%fastfourrier
+%TODO other lines
+Fn = Fs/2;                                          % Nyquist Frequency
+L = numel(emg(:,1));                                % Signal Length
+FTs = fft(emg(:,1))/L;                              % Fourier Transform first column
+Fv = linspace(0, 1, fix(L/2)+1)*Fn;                 % Frequency Vector
+
+%% parametrized band-pass filter
+FTs = bandpass(FTs, [lowerbandpass,upperbandpass], Fs);
+%% 
+Iv = 1:numel(Fv);                                   % Index Vector
+figure
+plot(Fv, abs(FTs(Iv))*2)
+grid
+xlabel('Frequency')
+ylabel('Amplitude')
 
